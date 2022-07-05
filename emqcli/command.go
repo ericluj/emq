72b48ea32e184246766f4d6905e905ee456514e2
ericluj <847751875@qq.com>
2022-07-05 23:10:02 +0800

    producer

--- emqcli/command.go
+++ emqcli/command.go
@@ -1,5 +1,15 @@
 package emqcli
 
+import (
+	"encoding/binary"
+	"io"
+)
+
+var (
+	byteSpace   = []byte(" ")
+	byteNewLine = []byte("\n")
+)
+
 type Command struct {
 	Name   []byte
 	Params [][]byte
@@ -9,4 +19,64 @@ type Command struct {
 func Subscribe(topic string, channel string) *Command {
 	var params = [][]byte{[]byte(topic), []byte(channel)}
 	return &Command{[]byte("SUB"), params, nil}
+}
+
+func Publish(topic string, body []byte) *Command {
+	var params = [][]byte{[]byte(topic), body}
+	return &Command{[]byte("PUB"), params, nil}
+}
+
+func (cmd *Command) WriteTo(w io.Writer) (int64, error) {
+	var (
+		total int64
+		err   error
+		buf   [4]byte
+	)
+
+	// 发送命令名
+	n, err := w.Write(cmd.Name)
+	total += int64(n)
+	if err != nil {
+		return total, err
+	}
+
+	// 发送参数
+	for _, param := range cmd.Params {
+		n, err := w.Write(byteSpace) // 空格分开
+		total += int64(n)
+		if err != nil {
+			return total, err
+		}
+		n, err = w.Write(param)
+		total += int64(n)
+		if err != nil {
+			return total, err
+		}
+	}
+
+	// 发送消息内容前换行
+	n, err = w.Write(byteNewLine)
+	total += int64(n)
+	if err != nil {
+		return total, err
+	}
+
+	// 发送消息内容
+	if cmd.Body != nil {
+		bufs := buf[:]
+		// 大端字节序发送内容长度
+		binary.BigEndian.PutUint32(bufs, uint32(len(cmd.Body)))
+		n, err = w.Write(bufs)
+		total += int64(n)
+		if err != nil {
+			return total, err
+		}
+		// 发送消息内容
+		n, err = w.Write(cmd.Body)
+		total += int64(n)
+		if err != nil {
+			return total, err
+		}
+	}
+	return total, nil
 }
