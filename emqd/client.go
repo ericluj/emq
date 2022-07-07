8367703b1b74abcbf1f18dcaaffc134b7d9fdcd9
ericluj <847751875@qq.com>
2022-07-07 17:25:18 +0800

    fix protocol

--- emqd/client.go
+++ emqd/client.go
@@ -21,8 +21,9 @@ type Client struct {
 	Reader *bufio.Reader
 	Writer *bufio.Writer
 
-	Channel *Channel // client订阅的channel
-	State   int32
+	Channel      *Channel      // client订阅的channel
+	SubEventChan chan *Channel // 事件，说明client有订阅
+	State        int32
 
 	ExitChan chan int
 	lenBuf   [4]byte
