59636d8d56cb6fa8faeca9f57f787c2bdae2c185
ericluj <847751875@qq.com>
2022-07-21 22:46:51 +0800

    emqd protocol interface

--- emqd/tcp.go
+++ emqd/tcp.go
@@ -6,6 +6,8 @@ import (
 	"sync"
 
 	log "github.com/ericluj/elog"
+	"github.com/ericluj/emq/internal/common"
+	"github.com/ericluj/emq/internal/protocol"
 )
 
 type TCPServer struct {
@@ -27,15 +29,25 @@ func (t *TCPServer) Handle(conn net.Conn) {
 
 	// 判断协议是否正确 TODO: 这里可以通过interface{}方式支持多个协议版本
 	pm := string(buf)
-	if ProtoMagic != pm {
+	if common.ProtoMagic != pm {
 		log.Infof("client(%s) bad protocol magic '%s'", conn.RemoteAddr(), pm)
 		conn.Close()
 		return
 	}
 	log.Infof("CLIENT(%s): desired protocol magic '%s'", conn.RemoteAddr(), pm)
 
-	prot := &Protocol{emqd: t.emqd}
-	client := prot.NewClient(conn, t.emqd)
+	var prot protocol.Protocol
+	switch pm {
+	case common.ProtoMagic:
+		prot = &Protocol{emqd: t.emqd}
+	default:
+		protocol.SendFramedResponse(conn, common.FrameTypeError, common.BadProtocolBytes)
+		conn.Close()
+		log.Infof("client(%s) bad protocol magic '%s'", conn.RemoteAddr(), pm)
+		return
+	}
+
+	client := prot.NewClient(conn)
 	t.conns.Store(conn.RemoteAddr(), client)
 
 	// client处理工作
@@ -46,4 +58,11 @@ func (t *TCPServer) Handle(conn net.Conn) {
 
 	t.conns.Delete((conn.RemoteAddr()))
 	conn.Close()
+}
+
+func (s *TCPServer) Close() {
+	s.conns.Range(func(k, v interface{}) bool {
+		v.(protocol.Client).Close()
+		return true
+	})
 }
