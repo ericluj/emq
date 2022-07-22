3bebfe7461ad5f86e0d542ef2f60d5bceae79705
ericluj <847751875@qq.com>
2022-07-22 19:57:21 +0800

    register unregister

--- emqlookupd/protocol.go
+++ emqlookupd/protocol.go
@@ -63,10 +63,106 @@ func (l *LookupProtocol) Exec(client *Client, params []string) ([]byte, error) {
 	switch params[0] {
 	case "REGISTER":
 		return l.REGISTER(client, params)
+	case "UNREGISTER":
+		return l.UNREGISTER(client, params)
 	}
-	return nil, fmt.Errorf("invalid command %s", params[0])
+	return nil, fmt.Errorf("invalid command %s", params)
 }
 
 func (l *LookupProtocol) REGISTER(client *Client, params []string) ([]byte, error) {
+	if client.peerInfo == nil {
+		return nil, fmt.Errorf("client must IDENTIFY")
+	}
+
+	if len(params) < 2 {
+		return nil, fmt.Errorf("REGISTER insufficient number of parameters")
+	}
+
+	topic := params[1]
+	var channel string
+	if len(params) >= 2 {
+		channel = params[2]
+	}
+
+	producer := &Producer{peerInfo: client.peerInfo}
+	if channel != "" {
+		key := Registration{
+			Category: "channel",
+			Key:      topic,
+			SubKey:   channel,
+		}
+
+		if l.emqlookupd.DB.AddProducer(key, producer) {
+			log.Infof("DB: client(%s) REGISTER category:%s key:%s subkey:%s", client, "channel", topic, channel)
+		}
+	}
+
+	key := Registration{
+		Category: "topic",
+		Key:      topic,
+		SubKey:   "",
+	}
+	if l.emqlookupd.DB.AddProducer(key, producer) {
+		log.Infof("DB: client(%s) REGISTER category:%s key:%s subkey:%s", client, "topic", topic, "")
+	}
+
+	return common.OKBytes, nil
+}
+
+func (l *LookupProtocol) UNREGISTER(client *Client, params []string) ([]byte, error) {
+	if client.peerInfo == nil {
+		return nil, fmt.Errorf("client must IDENTIFY")
+	}
+
+	if len(params) < 2 {
+		return nil, fmt.Errorf("REGISTER insufficient number of parameters")
+	}
+
+	topic := params[1]
+	var channel string
+	if len(params) >= 2 {
+		channel = params[2]
+	}
+
+	if channel != "" {
+		key := Registration{
+			Category: "channel",
+			Key:      topic,
+			SubKey:   channel,
+		}
+
+		removed, left := l.emqlookupd.DB.RemoveProducer(key, client.peerInfo.id)
+		if removed {
+			log.Infof("DB: client(%s) UNREGISTER category:%s key:%s subkey:%s", client, "channel", topic, channel)
+		}
+
+		if left == 0 {
+			l.emqlookupd.DB.RemoveRegistration(key)
+		}
+
+	} else { // 不为空删除topic下所有channel
+		registrations := l.emqlookupd.DB.FindRegistrations("channel", topic, "*")
+		for _, r := range registrations {
+			if removed, _ := l.emqlookupd.DB.RemoveProducer(r, client.peerInfo.id); removed {
+				log.Infof("client(%s) unexpected UNREGISTER category:%s key:%s subkey:%s", client, "channel", topic, r.SubKey)
+			}
+		}
+
+		key := Registration{
+			Category: "topic",
+			Key:      topic,
+			SubKey:   "",
+		}
+		removed, left := l.emqlookupd.DB.RemoveProducer(key, client.peerInfo.id)
+		if removed {
+			log.Infof("DB: client(%s) UNREGISTER category:%s key:%s subkey:%s", client, "topic", topic, "")
+		}
+
+		if left == 0 {
+			l.emqlookupd.DB.RemoveRegistration(key)
+		}
+
+	}
+
 	return common.OKBytes, nil
 }
--- emqlookupd/registration_db.go
+++ emqlookupd/registration_db.go
@@ -1,14 +1,135 @@
 package emqlookupd
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
 type RegiostrationDB struct {
-	mtx sync.RWMutex
+	mtx             sync.RWMutex
+	registrationMap map[Registration]map[string]*Producer
 }
 
 type Registration struct {
+	Category string
+	Key      string
+	SubKey   string
+}
+
+func (k Registration) IsMatch(category, key, subKey string) bool {
+	if category != k.Category {
+		return false
+	}
+	if key != "*" && key != k.Key {
+		return false
+	}
+	if subKey != "*" && subKey != k.SubKey {
+		return false
+	}
+	return true
+}
+
+type PeerInfo struct {
+	lastUpdate       int64
+	id               string
+	RemoteAddress    string
+	HostName         string
+	BroadcastAddress string
+	TCPPort          int
+	HTTPPort         int
+}
+
+type Producer struct {
+	peerInfo     *PeerInfo
+	tombstoned   bool
+	tombstonedAt time.Time
 }
 
 func NewRegiostrationDB() *RegiostrationDB {
-	return nil
+	return &RegiostrationDB{
+		registrationMap: make(map[Registration]map[string]*Producer, 0),
+	}
+}
+
+func (r *RegiostrationDB) AddRegistration(k Registration) {
+	r.mtx.Lock()
+	defer r.mtx.Unlock()
+
+	_, ok := r.registrationMap[k]
+	if !ok {
+		r.registrationMap[k] = make(map[string]*Producer)
+	}
+}
+
+func (r *RegiostrationDB) RemoveRegistration(k Registration) {
+	r.mtx.Lock()
+	defer r.mtx.Unlock()
+	delete(r.registrationMap, k)
+}
+
+func (r *RegiostrationDB) AddProducer(k Registration, p *Producer) bool {
+	r.mtx.Lock()
+	defer r.mtx.Unlock()
+
+	if _, ok := r.registrationMap[k]; !ok {
+		r.registrationMap[k] = make(map[string]*Producer)
+	}
+
+	producers := r.registrationMap[k]
+	if _, found := producers[p.peerInfo.id]; !found {
+		producers[p.peerInfo.id] = p
+		return true
+	}
+
+	return false
+}
+
+func (r *RegiostrationDB) RemoveProducer(k Registration, id string) (bool, int) {
+	r.mtx.Lock()
+	defer r.mtx.Unlock()
+
+	producers, ok := r.registrationMap[k]
+	if !ok {
+		return false, 0
+	}
+
+	if _, found := producers[id]; found {
+		delete(producers, id)
+		return true, len(producers)
+	}
+
+	return false, len(producers)
+}
+
+func (r *RegiostrationDB) FindRegistrations(category, key, subKey string) []Registration {
+	r.mtx.Lock()
+	defer r.mtx.Unlock()
+
+	if !r.needFilter(key, subKey) {
+		k := Registration{
+			Category: category,
+			Key:      key,
+			SubKey:   subKey,
+		}
+
+		if _, ok := r.registrationMap[k]; ok {
+			return []Registration{k}
+		}
+
+		return []Registration{} // 没有搜到
+	}
+
+	// 需要过滤出所有的符合通配符
+	results := make([]Registration, 0)
+	for k := range r.registrationMap {
+		if !k.IsMatch(category, key, subKey) {
+			continue
+		}
+		results = append(results, k)
+	}
+	return results
+}
+
+func (r *RegiostrationDB) needFilter(key, subKey string) bool {
+	return key == "*" || subKey == "*"
 }
